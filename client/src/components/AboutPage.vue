4b8129daf522218b44b453dd949e8df7f2e874f9
ShvydkoKristina <kristinashvidko@gmail.com>
2026-05-07 11:15:38 +0300

    Перший коміт

---
+++ client/src/components/AboutPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">About Web-Site</h1>
+    <div class="card shadow p-4">
+      <h3>Logo</h3>
+      <img src="/SKR.svg" alt="logo" width="90" />
+      <h3>Про сайт "AddContacts"</h3>
+      <p>
+        Веб-сайт "AddContacts" є телефонним довідником, призначеним для зручного
+        збереження, перегляду та керування контактною інформацією користувачів.
+      </p>
+      <h3>Основні можливості</h3>
+      <p>
+        Сайт дозволяє додавати нові контакти, редагувати існуючі записи,
+        видаляти контакти та сортувати список.
+      </p>
+      <h3>Автор</h3>
+      <p>Shvydko Kristina</p>
+    </div>
+  </div>
+</template>
